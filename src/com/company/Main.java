7618cad34bc1113aa691bb6b3ab8170a8e8623f2
KanatUbraimov <ubrajimovkanatbek520@gmail.com>
2022-01-31 15:59:14 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int a = 87236432;   // int - для чисел
+        String s = "6432";   // String - для букв
+/*
+        final int age;       //объявление переменной     final - не дает иенять значение
+        age = 47;          // инициализация
+*/
+/*
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+
+        boolean opened = true;
+
+        if (age < 30) {
+            System.out.println("isnt opened");
+        } else if (age < 10 || age > 30) {      //      || - условный оператор "или" , && - условный оператор "и"
+            System.out.println("its opened");
+        } else {
+            System.out.println("end");
+        }
+
+    }
+*/
+        callSomeBody("Aibek");
+        callSomeBody("Max");
+        System.out.println(kvagratChisla(5, 6));
+
+    }
+
+    public static void gotovitEdu() {                // метод без входящих параметров
+        System.out.println("нарезано");
+        System.out.println("сварили");
+        System.out.println("Накрыли стол");
+    }
+
+    public static void callSomeBody(String name) {          // метод с входящим параметром
+        System.out.println("Позовите " + name);
+    }
+
+    public static int kvagratChisla(int number, int number2) {          // возвращаемый метод
+        int result = number * number2;
+        return result;
+    }
+
+
+}
